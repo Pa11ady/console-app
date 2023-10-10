9f64003d4d3ca84ea3804feda718d1172d948044
Pavel <pa11ady@yandex.ru>
2023-10-10 19:55:49 +0300

    MemoryRepositorys

---
+++ wallet-service/src/main/java/ru/ylab/wallet/infrastructure/storage/memory/MemoryEventRepository.java
@@ -0,0 +1,31 @@
+package ru.ylab.wallet.infrastructure.storage.memory;
+
+import ru.ylab.wallet.domain.EventRepository;
+import ru.ylab.wallet.domain.model.Event;
+import ru.ylab.wallet.domain.model.EventType;
+
+import java.time.OffsetDateTime;
+import java.util.*;
+
+public class MemoryEventRepository implements EventRepository {
+   private final Map<UUID, Event> eventMap = new HashMap<>();
+
+    @Override
+    public Event createEvent(Event event) {
+        Event copy = new Event(
+                event.getId(),
+                event.getType(),
+                event.getUserId(),
+                event.getUserLogin(),
+                event.getEventDate(),
+                event.getDescription()
+        );
+        eventMap.put(event.getId(), copy);
+        return event;
+    }
+
+    @Override
+    public List<Event> findAll() {
+        return new ArrayList<>(eventMap.values());
+    }
+}
